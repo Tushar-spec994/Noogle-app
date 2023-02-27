919ce0a196bdd247068f96e126c1a3bd8fd07590
Tushar Das <tushar.das9407@gmail.com>
2023-02-27 12:32:06 +0530

    Folders Upload

---
+++ src/contexts/StateContextProvider.jsx
@@ -0,0 +1,36 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const StateContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getResults = async (type) => {
+    setLoading(true);
+
+    const res = await fetch(`${baseUrl}${type}`, {
+      method: 'GET',
+      headers: {
+        'X-User-Agent': 'desktop',
+        'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
+        'X-RapidAPI-Key': '7219183ca1msh6713fa0840275dbp12a7bejsn4e14b77850e9'
+      },
+    });
+
+    const data = await res.json();
+
+    setResults(data);
+    setLoading(false);
+  };
+
+  return (
+    <StateContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, loading }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
